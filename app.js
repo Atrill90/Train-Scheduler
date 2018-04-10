2cc84bd9baf0953d38a80b24bf56f09c16fd9684
Alex Trilling <atrill90@gmail.com>
2018-04-10 17:59:57 -0400

    my train schedule updates from the firebase server

--- app.js
+++ app.js
@@ -16,13 +16,45 @@ $(document).ready(function () {
 
     $("#submit").on("click", function (e) {
         e.preventDefault();
-       
+
         trainName = $("#nameInput").val().trim();
         trainDest = $("#destinationInput").val().trim();
         trainTime = $("#timeInput").val().trim();
         nextTrain = $("#frequencyInput").val().trim();
+        console.log(trainName);
+        console.log(trainDest);
+        console.log(trainTime);
+        console.log(nextTrain);
+        database.ref().push({
+
+            name: trainName,
+            destination: trainDest,
+            firstDeparture: trainTime,
+            nextTrain: nextTrain,
+        });
 
 
+    });
 
+    database.ref().on("child_added", function (childSnapshot, ) {
+        let newTRow = $("<tr>");
+        let trainHolder = $("<td>");
+        let trainDestHolder = $("<td>");
+        let trainTimeHolder = $("<td>");
+        let nextTrainHolder = $("<td>");
+
+        newTRow.append(trainHolder, trainDestHolder, trainTimeHolder, nextTrainHolder,);
+        $("#stats").append(newTRow);
+
+        let trainName = "";
+        let trainRole = "";
+        let trainSD = "";
+        let nextTrain = "";
+
+
+        $(trainHolder).html(childSnapshot.val().name);
+        $(trainDestHolder).html(childSnapshot.val().destination);
+        $(trainTimeHolder).html(childSnapshot.val().firstDeparture);
+        $(nextTrainHolder).html(childSnapshot.val().nextTrain);
     });
 });
